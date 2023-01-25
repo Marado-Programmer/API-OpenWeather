75b730c11296c78082fefc736cf4c3526ad572d6
João Rodrigues <jafonsoarodrigues@gmail.com>
2023-01-25 16:25:39 +0000

    '-'

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ const api = 'http://api.openweathermap.org/data/2.5/weather';
 //* usar CORS proxy para aceder aos dados da api
 const proxy = 'https://cors-anywhere.herokuapp.com/'
 //* in the variable key, use your openweather key, you can obtain it here https://openweathermap.org/
-const key = '7b1013440267dc18a3e3de3646adda17';
+const key = 'your key';
 //* seleciona o elemento form
 const form = document.querySelector('form.search');
 //* seleciona o element div
